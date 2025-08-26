687358ae912c57a1811365e7daec94e228c368ae
jeonkyujin <954qew@naver.com>
2025-08-27 02:50:16 +0900

    [chore] : 테스트

--- src/main/java/org/danji/global/config/RootConfig.java
+++ src/main/java/org/danji/global/config/RootConfig.java
@@ -76,7 +76,7 @@ public class RootConfig {
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
         config.addDataSourceProperty("useServerPrepStmts", "true");
-        config.setMaximumPoolSize(9);
+        config.setMaximumPoolSize(8);
         config.setMinimumIdle(4);
 
         HikariDataSource dataSource = new HikariDataSource(config);
